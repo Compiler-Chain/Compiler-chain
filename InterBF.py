b6a5c15740a78548e78b503521f5ea2c8a04903b
Destructible-Watermelon <Halfling.wizard123@gmail.com>
2017-06-26 20:21:07 +0930

    InterBF interpreter made, readme made.

---
+++ InterBF.py
@@ -0,0 +1,96 @@
+class InterBFInterpreter:
+    def __init__(self, program):
+        import re
+        program = program[:program.index("!")]
+        program = re.sub(r"[^+\-<>.,\[\]]", "", program)
+        self.input = None
+        self.input_index = 0
+        self.run(self.parse(program), [0], 0)
+    def parse(self, program_string):
+        i=0
+        parsed_prog = []
+        while i < len(program_string):
+            if program_string[i] == ",":
+                parsed_prog.append(0)
+                i += 1
+            elif program_string[i] == ".":
+                parsed_prog.append(1)
+                i += 1
+            elif program_string[i] in "+-><":
+                effects = {}
+                offset = 0
+                while i < len(program_string) and program_string[i] in "+-><":
+                    if program_string[i] == "+":
+                        if offset in effects:
+                            effects[offset] += 1
+                        else:
+                            effects[offset] = 1
+                    elif program_string[i] == "-":
+                        if offset in effects:
+                            effects[offset] -= 1
+                        else:
+                            effects[offset] = -1
+                    elif program_string[i] == ">":
+                        offset += 1
+                    else:
+                        offset -= 1
+                    i += 1
+                parsed_prog.append((0,)+ tuple(effects.items()) + (offset,))
+
+            elif program_string[i] == "[":
+                loop_start = i+1
+                scope = 1
+                while scope > 0:
+                    i+=1
+                    if program_string[i] == "[":
+                        scope += 1
+                    elif program_string[i] == "]":
+                        scope -= 1
+                parsed_prog.append((1,) + self.parse(program_string[loop_start:i]))
+                i += 1
+        return tuple(parsed_prog)
+
+    def run(self, program, tape, tape_pointer):
+        for i in program:
+            if i == 0:
+                if self.input is None:
+                    self.input = input()
+                    tape[tape_pointer] = ord(self.input[0])
+                    self.input_index = 1
+                elif self.input_index==len(self.input):
+                    self.input_index = 0
+                    self.input = None
+                    tape[tape_pointer] = 0
+                else:
+                    tape[tape_pointer] =  ord(self.input[self.input_index])
+                    self.input_index += 1
+
+            elif i == 1:
+                print(chr(tape[tape_pointer]),end = "")
+            elif i[0] == 0:
+                for cell_change in i[1:-1]:
+                    cells_from_right = (len(tape)-(tape_pointer + 1))
+                    if cell_change[0]-cells_from_right > 0:
+                        tape.extend([0]*(cell_change[0]-cells_from_right))
+                    tape[tape_pointer+cell_change[0]] += cell_change[1]
+                    tape[tape_pointer+cell_change[0]] %= 256
+                tape_pointer += i[-1]
+                if tape_pointer >= len(tape):
+                    tape.extend([0]*(tape_pointer-len(tape)+1))
+            elif i[0] == 1:
+                while tape[tape_pointer]:
+                    tape, tape_pointer = self.run(i[1:], tape, tape_pointer)
+
+        return tape, tape_pointer
+
+
+
+
+if __name__ == "__main__":
+    in_program = []
+    line = ""
+    while "!" not in line:
+        line = input()
+        in_program.append(line)
+    in_program = ''.join(in_program)
+    InterBFInterpreter(in_program)
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Compiler-chain
 A chain of compilers across various new and precreated languages, it should be fun.
+
+# Wot u do:
+Make a compiler in the most recent language, from a new language to the language the compiler is written in. In this way, a compiler chain will be formed
+
+
+# Rules:
+You may not make a compiler for a previously used language
+Your language must have interactive io
+Your language must be Turing complete.
+Your language compiler must not be turing complete on the assumption that the numbers from the previous language are arbitrarily large, when they are not
+Please don't bungle up stuff by submitting multiple compilers in the same language.
+Give others a chance to get a language in there: if you have just submitted a language, wait until some time has passed, or someone else has made an interpreter
+
+
+## Rules on premade langs:
+
+I guess it would be cool to have some, but I think that a large amount of the languages should be newly made.
