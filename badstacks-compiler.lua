15871d5fbf2a99a9b78538f9e986ad6e1ba37e8a
William Lemon <William.Lemon2@gmail.com>
2017-06-28 11:35:14 +1000

    Added Badstacks BF compiler, updated lua compiler
    The Lua version's entire job is to write a BF compiler, written in
    itself. Because bootstrapping's the name of the game.

--- badstacks-compiler.lua
+++ badstacks-compiler.lua
@@ -12,8 +12,11 @@ local funcs = {
 	DEC = "-",
 	INC = "+",
 	ZERO = "[-]",
+	NEGATE = "[->+<]>[-<->]<",
 	STACKADD = "[-<<<<+>>>>]<-<<<",
-	STACKSUB = "[-<<<<->>>>]<-<<<"
+	STACKSUB = "[-<<<<->>>>]<-<<<",
+	STACKMULT = "<<<<[->>>>[->>>+>+<<<<]>>>[-<<<+>>>]<<<<<<<]>>>>>>>>[-<<<<<<<<+>>>>>>>>]<<<<[-]<-<<<",
+	NOT = ">>>+<<<[[-]>>>-<<<]>>>[-<<<+>>>]<<<"
 }
 
 setmetatable(_G, {__index = funcs})
@@ -21,11 +24,26 @@ funcs.MOVE = FLIP .. PUSH .. FLIP .. "[-" .. FLIP .. "+" .. FLIP .. "]" .. POP .
 funcs.COPY = PEEK .. MOVE
 funcs.READ = PUSH .. ","
 funcs.ADD = FLIP .. COPY .. STACKADD
-funcs.NEGATE = "[->+<]>[-<->]<"
 funcs.SUB = FLIP .. COPY .. STACKSUB
+funcs.MULT = FLIP .. COPY .. STACKMULT
+local charNames = {p=PUSH, P=POP,f=FLIP,c=PEEK,["["]=WHILE,["]"]=END,["."]=WRITE,[","]=READ,["?"]=RAND,["-"]=DEC,["+"]=INC,z=ZERO,n=NEGATE,a=STACKADD,A=ADD,m=STACKMULT,M=MULT,s=STACKSUB,S=SUB,F=MOVE,C=COPY,["!"]=NOT,["0"]=PUSH,["1"]=PUSH.."+",["2"]=PUSH.."++",["3"]=PUSH.."+++",["4"]=PUSH.."++++",["5"]=PUSH.."+++++",["6"]=PUSH.."++++++",["7"]=PUSH.."+++++++",["8"]=PUSH.."++++++++",["9"]=PUSH.."+++++++++"}
+--local charNames = {p=PUSH}
+local inStr = "WRITE_THE_HEADER\n"
+for s in (">>>>+>>+>"):gmatch"." do
+	inStr = inStr .. string.byte(s) .. " WRITE "
+end
+inStr = inStr .. "\nREAD WHILE\n"
+for name,func in pairs(charNames) do
+	inStr = inStr .. "\tPEEK "..string.byte(name).." STACKSUB NOT WHILE IF_CHAR_IS(" .. name .. ")\n\t\tWRITE("..func..")\n"
+	for s in func:gmatch"." do
+		inStr = inStr .. "\t\tPOP " .. string.byte(s) .. " WRITE\n"
+	end
+	inStr = inStr .. "\t\tPOP 0\n\tEND POP\n"
+end
 
-local inStr = "80 3 STACKSUB"
+inStr = inStr .. "\tPOP READ\nEND"
 
+print(inStr)
 for S in inStr:gmatch"%S+" do
 	if tonumber(S) then
 		s = s .. funcs.PUSH .. ("+"):rep(tonumber(S))
